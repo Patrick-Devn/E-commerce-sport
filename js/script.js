48407a3141bbf84cfd707baf59d354547f3cef34
griffinphat <laithuanphat@gmail.com>
2025-04-28 13:30:58 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// js/script.js
+console.log("Script loaded!");
+
+// Example: You might add code later for things like:
+// - A mobile navigation menu toggle
+// - Image sliders/carousels
+// - Filtering products without reloading (more advanced)
+// - Form validation
